5fc376f6d17eb3ee3c40dba7574482b1c247babc
Nisar Abed <59590049+nisar86@users.noreply.github.com>
2021-03-16 01:30:27 +0100

    Add files via upload

---
+++ assets/game/js/jsr-soundtrack.js
@@ -0,0 +1,22 @@
+//=======================================================
+// JSR Soundtrack > JS RACER TURBO
+//=======================================================
+/* 
+ * JS Racer Turbo (Racing Game)
+ * Copyright (c) 2020, 2021 Nisar Abed and Contributors
+ * Licensed under the Creative Commons Attribution 4.0 International (CC BY 4.0).
+ * License: https://creativecommons.org/licenses/by/4.0/legalcode
+ * 
+ * Author: Nisar Abed (https://www.nisar.it)
+ * Play the game: https://www.nisar.it/app/js-racer-turbo
+ * Repository:    https://github.com/nisar86/js-racer-turbo
+ */
+
+/*
+window.jsr_soundtrack = {
+    // Tracks
+    track1 : { chapter : 0  , title : 'Roborama - I Bet U Kunt Go Faster'              },
+    track2 : { chapter : 219, title : 'Ankubu - Sentinel'                              },
+    track3 : { chapter : 429, title : 'Serpe Terror - Balla con me fra (Instrumental)' }
+}
+*/
