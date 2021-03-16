37cd39c2e760339b87572a6f9baee891e4b6674a
Nisar Abed <59590049+nisar86@users.noreply.github.com>
2021-03-16 01:21:22 +0100

    Add files via upload

---
+++ assets/game/js/demo/run-demo.js
@@ -0,0 +1,21 @@
+//=======================================================
+// RUN > JS RACER TURBO > All Demos
+//=======================================================
+/* 
+ * JS Racer Turbo (Racing Game)
+ * Copyright (c) 2020, 2021 Nisar Abed and Contributors
+ * Licensed under the Creative Commons Attribution 4.0 International (CC BY 4.0).
+ * License: https://creativecommons.org/licenses/by/4.0/legalcode
+ * 
+ * Author: Nisar Abed (https://www.nisar.it)
+ * Play the game: https://www.nisar.it/app/js-racer-turbo
+ * Repository:    https://github.com/nisar86/js-racer-turbo
+ */
+
+(function($) {
+    "use strict";
+    $(document).ready(function() {
+        // Play the Game
+        window.RunRacer();
+    });
+})(jQuery);
---
+++ assets/game/js/jsr-localize.js
@@ -0,0 +1,50 @@
+//=======================================================
+// JSR Localize > JS RACER TURBO
+//=======================================================
+/* 
+ * JS Racer Turbo (Racing Game)
+ * Copyright (c) 2020, 2021 Nisar Abed and Contributors
+ * Licensed under the Creative Commons Attribution 4.0 International (CC BY 4.0).
+ * License: https://creativecommons.org/licenses/by/4.0/legalcode
+ * 
+ * Author: Nisar Abed (https://www.nisar.it)
+ * Play the game: https://www.nisar.it/app/js-racer-turbo
+ * Repository:    https://github.com/nisar86/js-racer-turbo
+ */
+
+/*
+window.jsr_localize = {
+    // Localize Texts
+    localize : {
+        // Stats
+        stats : {
+            label          : 'Performance',
+            label_good     : 'Good',
+            label_ok       : 'Ok',
+            label_bad      : 'Bad',
+            label_disabled : 'Stats Disabled'
+        },
+        // Game Over
+        game_over : {
+            title          : 'GAME OVER',
+            fastestlap     : 'Your total time is',
+            fastestlap2    : 's',
+            outoftime      : 'You have reached the',
+            outoftime2     : 'th lap',
+            restart_title  : 'Reload and Play Again',
+            restart_btn    : 'Play Again',
+            restart_canvas : 'Go out and play again',
+            result_1       : 'GREAT!',
+            result_2       : 'VERY GOOD!',
+            result_3       : 'GOOD!',
+            result_4       : 'POOR!',
+            result_5       : 'MAH!'
+        },
+        // Settings
+        settings : {
+            lap            : 'Lap',
+            laps           : 'Laps'
+        }
+    }
+};
+*/
